18f9214c212a725bc7f8e21cd5e4653b0fb019df
Drobysz <alexanderdrobyschevski@gmail.com>
2025-08-17 11:24:47 +0200

    * Add str_len * Add error * decompile_str

--- rush01/rush01.c
+++ rush01/rush01.c
@@ -1,12 +1,64 @@
-char	decompile_str(char *str)
+#include <unistdio.h>
+
+void	error(void)
 {
+	char *error;
+
+	error = "Error\n";
+	while (error)
+	{
+		write(1, error, 1);
+		error++;
+	}
 	
 }
 
-int	rush(char *str)
+char	decompile_str(char *str)
 {
-	char max_view_arr;
+	int	num_set[16];
+	int	i;
 
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= '0' || str[i] <= '9')
+		{
+			num_set[i] = str[i] - '0';
+		}
+		else if (str[i] != ' ')
+		{
+			break;
+		}
+		
+		i++;
+	}
+
+	return (num_set);
+}
+
+int	str_len(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	rush(char *str)
+{
+	int max_view_arr[16];
+
+	if (str_len(*str) != 31)
+	{
+		error();
+	}
+	
+	max_view_arr = decompile_str(*str);
 	
 }
 
